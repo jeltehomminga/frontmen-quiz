f578bb2fa644db6016cc10664115a2ee9cf56f01
Jelte Homminga <jeltehomminga@gmail.com>
2019-05-15 12:10:02 +0200

    Updates

---
+++ pages-manifest-e9e3602dd39fce719e3d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{144:function(e){e.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"},{componentChunkName:"component---src-pages-game-js",jsonName:"game-e74",path:"/game/"}],dataPaths:{"dev-404-page-5f9":"386/path---dev-404-page-5-f-9-fab-0SUcWyAf8ecbYDsMhQkEfPzV8","game-e74":"423/path---game-e-74-228-0SUcWyAf8ecbYDsMhQkEfPzV8",index:"140/path---index-6a9-0SUcWyAf8ecbYDsMhQkEfPzV8"}}}}]);
+//# sourceMappingURL=pages-manifest-e9e3602dd39fce719e3d.js.map
